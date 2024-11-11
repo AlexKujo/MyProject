eb1a05c7ca2bf60c05d504b96fc3f511fceb8bc9
AlexKujo <141486615+AlexKujo@users.noreply.github.com>
2024-11-11 21:01:39 +0300

    test

---
+++ MyProject NET 8/Apps/TextTemplates/Classes/ThreadType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyProject_NET_8.TextTemplates
+{
+    internal class ThreadType
+    {
+        public float MinValue { get; private set; }
+
+        public float MaxValue { get; private set; }
+
+        public string Designation { get; private set; }
+
+        public int WrenchSize { get; private set; }
+
+        [JsonConstructor]
+        public ThreadType(float minValue, float maxValue, string designation, int wrenchSize)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+            Designation = designation;
+            WrenchSize = wrenchSize;
+        }
+    }
+}
---
+++ MyProject NET 8/Lib/VersionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject_NET_8
+{
+    internal static class VersionInfo
+    {
+        public static string UpdateRequired { get; private set; } = "";
+        public static string NoUpdateRequired { get; private set; }
+
+        public static string GetCurrentVersion()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Version version = assembly.GetName().Version;
+
+            return version.ToString();
+        }
+    }
+}
